90c0d00a8270e8cd0183729e0c20220d088afc8e
Abhijit <abhijits30@gmail.com>
2016-11-28 13:48:31 +0530

    Revert "Insurance Policy Management"
    This reverts commit 21f6e47bc04f61a05e277437ef14c759499a2d56.

--- app1/app/models/plan.rb
+++
@@ -1,9 +0,0 @@
-class Plan < ApplicationRecord
-has_many :comments
-validates :plan_no,presence: true
-validates :plan_name,presence: true
-validates :company_name,presence: true
-validates :duration,presence: true
-validates :bonus,presence: true
-validates :roi,presence: true
-end
